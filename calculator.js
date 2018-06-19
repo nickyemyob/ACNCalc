620152b37734de8430f22de0275160653bfee029
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2018-06-19 12:27:03 +1200

    checks if ACN is valid

--- calculator.js
+++ calculator.js
@@ -1,5 +1,26 @@
+
 module.exports = {
-    calculateACN: (input) => {
-        return true;
+    calculateACN: (ACN) => {
+        const ACNToDigits = convertToDigits(ACN);
+        const numbersToCalculate = ACNToDigits.slice(0,ACNToDigits.length - 1);
+        const checkDigit = ACNToDigits[ACNToDigits.length - 1];
+
+        const reducer = (accumulator, currentValue, index) => {
+            let answer = accumulator + (currentValue * (index + 1));
+            return answer;
+        };
+        
+        const calculatedCheckDigit = numbersToCalculate.reverse().reduce(reducer, 0);
+        const remainder = calculatedCheckDigit % 10;
+        const complement = 10 - remainder;
+        return complement === checkDigit;
     }
 }
+
+const convertToDigits = taxNumber => {
+    const digits = taxNumber
+      .replace(/[^\d]/g, '')
+      .split('')
+      .map(Number);
+    return digits;
+  };
--- test.spec.js
+++ test.spec.js
@@ -1,10 +1,21 @@
 const calculator = require('./calculator.js');
 
-test('calculates correct ACN number', () => {
+test('should be true when ACN is correct', () => {
     const inputtedACN = '004 085 616';
     const isValid = true;
 
     const calcuatedACN = calculator.calculateACN(inputtedACN); 
 
+    // console.log(calcuatedACN);
     expect(calcuatedACN).toBeTruthy();
+});
+
+test('should be false when ACN is incorrect', () => {
+    const inputtedACN = '104 085 616';
+    const isValid = false;
+
+    const calcuatedACN = calculator.calculateACN(inputtedACN); 
+
+    // console.log(calcuatedACN);
+    expect(calcuatedACN).toBeFalsy();
 });
